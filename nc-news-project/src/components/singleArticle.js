19edb50c7c8902d6cc3a716a8f18f8a217a622ad
Michael Jones <michaeljones98@hotmail.co.uk>
2022-06-14 16:11:20 +0100

    Add isLoading

--- nc-news-project/src/components/singleArticle.js
+++ nc-news-project/src/components/singleArticle.js
@@ -1,19 +1,29 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { getSingleArticle } from "../api"
+import moment from "moment"
+import PropgateLoader from 'react-spinners/PropagateLoader'
 
 const SingleArticle = () => {
     const [articleArr, setArticleArr] = useState([{}])
     const {article_id} = useParams()
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         getSingleArticle(article_id).then(data => {
+            [data].map(article => {
+                let str = article.created_at
+                let date = moment(str).format("l")
+                return article.created_at = date
+            })
             setArticleArr([data])
+            setIsLoading(false)
         })
     }, [articleArr, article_id])
 
-   
+    const style = {position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }
 
+   if(isLoading) return <div style={style}><PropgateLoader /></div>
     return (
         <>
         <ul className="article-list">
