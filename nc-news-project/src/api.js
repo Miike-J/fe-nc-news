55e41537d294863d3e861c35f2ee1c857f43a9f8
Michael Jones <michaeljones98@hotmail.co.uk>
2022-06-14 14:10:55 +0100

    Can update article list with selected topic

--- nc-news-project/src/api.js
+++ nc-news-project/src/api.js
@@ -1,9 +1,21 @@
 const axios = require('axios')
 
-exports.getArticles = () => {
-    return axios.get('https://nc-news-project-app.herokuapp.com/api/articles').then(({data}) => {
+
+exports.getArticles = (topicSelect) => {
+    let url = 'https://nc-news-project-app.herokuapp.com/api/articles'
+    if(topicSelect) {
+        url += `?topic=${topicSelect}`
+    }
+    return axios.get(url).then(({data}) => {
         return data
     })
 }
+
+exports.getTopics = () => {
+    return axios.get('https://nc-news-project-app.herokuapp.com/api/topics').then(({data}) => {
+        return data 
+    })
+}
+
 
 
--- nc-news-project/src/components/Home.js
+++ nc-news-project/src/components/Home.js
@@ -1,30 +1,67 @@
 import { useState, useEffect } from "react"
-import {getArticles} from '../api'
+import {getArticles, getTopics} from '../api'
 import {Link} from 'react-router-dom'
 import moment from 'moment'
 import PropgateLoader from 'react-spinners/PropagateLoader'
+import Select from 'react-select'
 
 const Home = () => {
-    const [articleList, setArticleList] = useState([{}])
+    let [articleList, setArticleList] = useState([{}])
     const [isLoading, setIsLoading] = useState(true)
+    const [topicList, setTopicList] = useState([{}])
+    const [topicSelect, setTopicSelect] = useState('')
 
    useEffect(() => {
-    getArticles().then(data => {
+    getArticles(topicSelect).then(data => {
         data.articles.map(article => {
             let str = article.created_at
             let date = moment(str).format("l")
             return article.created_at = date
         })
+        
         setArticleList(data.articles)
         setIsLoading(false)
+
     })
    }, [articleList])
+
+   useEffect(() => {
+    getTopics().then(data => {
+        setTopicList(data.topics.map(topic => {
+            if(topic.slug){
+               return {value: topic.slug, label: (topic.slug.charAt(0).toUpperCase() + topic.slug.slice(1))} 
+            } else {
+                return topic
+            }
+        }))
+    })
+   }, [])
+
+   const handleSelectChange = (selectedOption) => {
+    setTopicSelect((currTopic) => {
+        if(!selectedOption){
+            return currTopic = ''
+        } else {
+            return currTopic = selectedOption.value
+        }
+    })
+   }
 
    const style = {position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }
 
    if(isLoading) return <div style={style}><PropgateLoader /></div>
     return (
+        <>
         <ul className="article-list">
+            <Select 
+            className="topic-select"
+            isClearable={true}
+            isSearchable={true}
+            name='topic'
+            placeholder='Topic...'
+            options={topicList}
+            onChange={handleSelectChange}
+            />
             {articleList.map(article => {
                 return (
                     <li className="single-article" key={article.article_id}>
@@ -38,6 +75,7 @@ const Home = () => {
                 )
             })}
         </ul>
+        </>
     )
 }
 
