7c9a675d4e8523e7ccaa12b3405518cf88ae03b8
Miike-J <michaeljones98@hotmail.co.uk>
2023-11-21 18:07:06 +0000

    update urls

--- nc-news-project/src/api.js
+++ nc-news-project/src/api.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 
 exports.getArticles = (topicSelect, sortSelect) => {
     //change url for .get if a topic is selected
-    let url = 'https://nc-news-project-app.herokuapp.com/api/articles'
+    let url = 'https://app-f2462bfb-8127-4237-8fc8-3e894771b534.cleverapps.io/api/articles'
     if(topicSelect && sortSelect.length === 0) {
         url += `?topic=${topicSelect}`
     } else if (sortSelect && topicSelect.length === 0) {
@@ -16,39 +16,39 @@ exports.getArticles = (topicSelect, sortSelect) => {
 }
 
 exports.getTopics = () => {
-    return axios.get('https://nc-news-project-app.herokuapp.com/api/topics').then(({data}) => {
+    return axios.get('https://app-f2462bfb-8127-4237-8fc8-3e894771b534.cleverapps.io/api/topics').then(({data}) => {
         return data 
     })
 }
 
 exports.getSingleArticle = (article_id) => {
-    return axios.get(`https://nc-news-project-app.herokuapp.com/api/articles/${article_id}`).then(data => {
+    return axios.get(`https://app-f2462bfb-8127-4237-8fc8-3e894771b534.cleverapps.io/api/articles/${article_id}`).then(data => {
         return data.data.article
     })
 }
 
 exports.updateVotes = (article_id, num) => {
-    return axios.patch(`https://nc-news-project-app.herokuapp.com/api/articles/${article_id}`, {inc_votes: num})
+    return axios.patch(`https://app-f2462bfb-8127-4237-8fc8-3e894771b534.cleverapps.io/api/articles/${article_id}`, {inc_votes: num})
 }
 
 exports.getArticleComments = (article_id) => {
-    return axios.get(`https://nc-news-project-app.herokuapp.com/api/articles/${article_id}/comments`).then(data => {
+    return axios.get(`https://app-f2462bfb-8127-4237-8fc8-3e894771b534.cleverapps.io/api/articles/${article_id}/comments`).then(data => {
         return data.data.comments
     })
 }
 
 exports.getUsers = () => {
-    return axios.get('https://nc-news-project-app.herokuapp.com/api/users').then(data => {
+    return axios.get('https://app-f2462bfb-8127-4237-8fc8-3e894771b534.cleverapps.io/api/users').then(data => {
         return data.data.users
     })
 }
 
 exports.postComment = (commentObj, article_id) => {
-    return axios.post(`https://nc-news-project-app.herokuapp.com/api/articles/${article_id}/comments`, commentObj).then(data => {
+    return axios.post(`https://app-f2462bfb-8127-4237-8fc8-3e894771b534.cleverapps.io/api/articles/${article_id}/comments`, commentObj).then(data => {
         return data.data.comment
     }).catch(console.dir)
 }
 
 exports.deleteComment = (comment_id) => {
-    return axios.delete(`https://nc-news-project-app.herokuapp.com/api/comments/${comment_id}`)
+    return axios.delete(`https://app-f2462bfb-8127-4237-8fc8-3e894771b534.cleverapps.io/api/comments/${comment_id}`)
 }
